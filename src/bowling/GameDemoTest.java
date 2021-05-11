fa683e489480c3cbdc66e248e664af67aff58cba
ethicnology <ethicnology@pm.me>
2021-05-11 17:57:55 -0400

    Setup JUnit 4

---
+++ src/bowling/GameDemoTest.java
@@ -0,0 +1,24 @@
+package bowling;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameDemoTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertEquals(0,0);
+	}
+
+}
