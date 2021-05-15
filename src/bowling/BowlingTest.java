66137f384adb6a97328c56d3e1b533a9008a7df0
ethicnology <ethicnology@pm.me>
2021-05-15 15:29:05 -0400

    comply to sun functions naming convention

--- src/bowling/BowlingTest.java
+++ src/bowling/BowlingTest.java
@@ -1,18 +1,20 @@
 package bowling;
-import stev.bowling.*;
+
 
 import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import stev.bowling.*;
 
 /**
- * Some tests.
+ * 
  * @author ethicnology.
  */
 public class BowlingTest {
 	private Game game;
-	
+
 	/**
 	 * Initialize a global Game variable.
 	 */
@@ -22,40 +24,41 @@ public class BowlingTest {
 	}
 
 	/**
-	 * NormalFrameSuccess should success with valid integers, between 1 to 9.  
+	 * NormalFrameSuccess should success with valid integers, between 1 to 9.
 	 */
 	@Test
 	public void NormalFrameSuccess() {
-        assertTrue(new NormalFrame(1) instanceof Frame);
-        assertTrue(new NormalFrame(2) instanceof Frame);
-        assertTrue(new NormalFrame(3) instanceof Frame);
-        assertTrue(new NormalFrame(4) instanceof Frame);
-        assertTrue(new NormalFrame(5) instanceof Frame);
-        assertTrue(new NormalFrame(6) instanceof Frame);
-        assertTrue(new NormalFrame(7) instanceof Frame);
-        assertTrue(new NormalFrame(8) instanceof Frame);
-        assertTrue(new NormalFrame(9) instanceof Frame);
+		assertTrue(new NormalFrame(1) instanceof Frame);
+		assertTrue(new NormalFrame(2) instanceof Frame);
+		assertTrue(new NormalFrame(3) instanceof Frame);
+		assertTrue(new NormalFrame(4) instanceof Frame);
+		assertTrue(new NormalFrame(5) instanceof Frame);
+		assertTrue(new NormalFrame(6) instanceof Frame);
+		assertTrue(new NormalFrame(7) instanceof Frame);
+		assertTrue(new NormalFrame(8) instanceof Frame);
+		assertTrue(new NormalFrame(9) instanceof Frame);
 	}
-	
+
 	/**
-	 * NormalFrameFailure should throw a BowlingException with a non-valid integer below 1 or over 9 (10 should works only with LastFrame).  
-	 */	
+	 * NormalFrameFailure should throw a BowlingException with a non-valid integer
+	 * below 1 or over 9 (10 should works only with LastFrame).
+	 */
 	@Test(expected = BowlingException.class)
 	public void NormalFrameFailure() throws Exception {
 		new NormalFrame(0);
 		new NormalFrame(10); // Should fail -> LastFrame
 	}
-	
+
 	/**
-	 * LastFrameSuccess should success with 10 a.k.a. last frame.  
+	 * LastFrameSuccess should success with 10 a.k.a. last frame.
 	 */
 	@Test
 	public void LastFrameSuccess() {
-        assertTrue(new LastFrame(10) instanceof Frame);
+		assertTrue(new LastFrame(10) instanceof Frame);
 	}
-	
+
 	/**
-	 * LastFrameException should throw exception, only acceptable value is 10.  
+	 * LastFrameException should throw exception, only acceptable value is 10.
 	 */
 	@Test(expected = BowlingException.class)
 	public void LastFrameFailure() throws Exception {
